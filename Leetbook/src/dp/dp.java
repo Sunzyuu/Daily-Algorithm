775d4fa8cc2979fdc32bca22c648ced897c90da9
sunzhengyu99 <sunzy9901@qq.com>
2022-09-13 21:31:21 +0800

    更新:dp增加1143

--- Leetbook/src/dp/dp.java
+++ Leetbook/src/dp/dp.java
@@ -596,7 +596,8 @@ public class dp {
      */
     public int findLength(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length + 1][nums2.length + 1];
-
+        // dp[i][j] ：以下标i - 1为结尾的A，和以下标j - 1为结尾的B，最长重复子数组长度为dp[i][j]。
+        // （特别注意： “以下标i - 1为结尾的A” 标明一定是 以A[i-1]为结尾的字符串 ）
         int res = 0;
         for (int i = 1; i <= nums1.length; i++) {
             for (int j = 1; j <= nums2.length; j++) {
@@ -609,6 +610,51 @@ public class dp {
         }
         return res;
     }
+
+    public int findLength1(int[] nums1, int[] nums2) {
+        int[] dp = new int[nums1.length + 1];
+        int res = 0;
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = nums2.length; j > 0; j--) {
+                if(nums1[i-1] == nums2[j - 1]){
+                    dp[j] = dp[j -1] + 1;
+                }else{
+                    dp[j] = 0;
+                }
+
+                if(res < dp[j]) res = dp[j];
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 1143. 最长公共子序列
+     * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+     * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+     * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for (int i = 1; i <= text1.length() ; i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[text1.length()- 1][text2.length() -1];
+    }
+
+
 
 
     public static void main(String[] args) {
