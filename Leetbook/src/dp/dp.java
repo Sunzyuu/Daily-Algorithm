68f4b1e3c89cd5407cb9dc4dc010ebc006a7fb8b
sunzhengyu99 <sunzy9901@qq.com>
2022-09-15 12:45:39 +0800

    更新:dp增加583

--- Leetbook/src/dp/dp.java
+++ Leetbook/src/dp/dp.java
@@ -803,6 +803,41 @@ public class dp {
         return dp[t.length()][s.length()];
     }
 
+    /**
+     * 583. 两个字符串的删除操作
+     * 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+     * 每步 可以删除任意一个字符串中的一个字符。
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        // dp[i][j]以i-1为结尾的字符串word1，和以j-1位结尾的字符串word2，想要达到相等，所需要删除元素的最少次数。
+
+        // 初始化
+        // dp[0][j]是指 word1为空字符串，那么长j-1的word2删除到空字符串的次数自然为j
+        // dp[i][0]同上
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= word1.length() ; i++) {
+            for (int j = 1; j <=word2.length() ; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j] + 1,dp[i][j-1] + 1), dp[i-1][j-1] + 2);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+
+    }
+
 
     public static void main(String[] args) {
         int[] weight = {1, 3, 4};
