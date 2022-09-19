ebe414077db31ef324c78a21bc5a30c51b42db9e
sunzhengyu99 <sunzy9901@qq.com>
2022-09-19 11:24:56 +0800

    更新： 34

--- EveryDay/src/DayEleven.java
+++ EveryDay/src/DayEleven.java
@@ -113,6 +113,40 @@ public class DayEleven {
         }
         return -1;
     }
+
+    /**
+     * 34. 在排序数组中查找元素的第一个和最后一个位置
+     * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+     * @param nums
+     * @param target
+     * @return
+     */
+    //先找>=target的第一个
+    //再找>target的第一个
+    //我真是这辈子都不想看见这题
+    public int[] searchRange(int[] nums, int target) {
+        int l=search(nums,target);
+        int r=search(nums,target+1);
+        if(l==nums.length||nums[l]!=target)
+            return new int[]{-1,-1};
+        return new int[]{l,r-1};
+    }
+    //找>=target的第一个
+    public int search(int[] nums,int target){
+        int l=0,r=nums.length;
+        while(l<r){
+            int mid=(r+l)>>1;
+            if(nums[mid]>=target)
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+
+
     public static void main(String[] args) {
 
     }
