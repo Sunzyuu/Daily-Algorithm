0cc1915dcc0ffe9b08762e7cf79c26ef1cc283c8
sunzhengyu99 <sunzy9901@qq.com>
2022-09-20 13:00:47 +0800

    更新：随机922

--- EveryDay/src/DayEleven.java
+++ EveryDay/src/DayEleven.java
@@ -113,6 +113,39 @@ public class DayEleven {
         }
         return -1;
     }
+
+    /**
+     * 922. 按奇偶排序数组 II
+     * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
+     * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。
+     * 你可以返回 任何满足上述条件的数组作为答案 。
+     * @param nums
+     * @return
+     */
+    public int[] sortArrayByParityII(int[] nums) {
+
+        int[] odd = new int[nums.length / 2];
+        int[] even = new int[nums.length / 2];
+
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] % 2 == 0){
+                even[index1++] = nums[i];
+            }else {
+                odd[index2++] = nums[i];
+            }
+        }
+
+        int resultIndex = 0;
+        for (int i = 0; i < nums.length / 2; i++) {
+            nums[resultIndex ++] = even[i];
+            nums[resultIndex ++] = odd[i];
+        }
+        return nums;
+    }
+
+
     public static void main(String[] args) {
 
     }
