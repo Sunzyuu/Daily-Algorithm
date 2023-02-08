76ce22cdf19f977429657691596a32ba2b54c455
sunzhengyu99 <sunzy9901@qq.com>
2023-02-08 23:38:34 +0800

    更新: 修改gitignore

--- Algorithm_course/src/greedy/hellword.java
+++
@@ -1,8 +0,0 @@
-package greedy;
-
-public class hellword {
-
-    public static void main(String[] args) {
-        System.out.println("孙正雨 " + " hello world");
-    }
-}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Daily-Algorithm
 leetcode刷题记录
 坚持每日打卡 2022.9.3
+
+开学了，再次捡起来
